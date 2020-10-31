35896c3ff4572c94c4baca69349ebffb7f7a374a
VirtCode <41426325+VirtCode@users.noreply.github.com>
2020-10-31 15:33:04 +0100

    Instructions

---
+++ paths/instructions.ts
@@ -0,0 +1,27 @@
+class Instruction {}
+
+class DriveInstruction extends Instruction {
+    private length : number;
+
+    constructor(length : number){
+        super();
+        this.length = length;
+    }
+
+    public getLength() : number {
+        return this.length;
+    }
+}
+
+class SteerInstruction extends DriveInstruction {
+    private angle : number;
+
+    constructor(length : number, angle : number){
+        super(length);
+        this.angle = angle;
+    }
+
+    public getAngle() : number {
+        return this.angle;
+    }
+}
